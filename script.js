cdcf430bd7e30549d73f7d64a00db7bbeef895ec
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-21 14:01:44 -0600

    Display the message 'Correct Number' if the user's guess is right

--- script.js
+++ script.js
@@ -16,5 +16,7 @@ document.querySelector(".check").addEventListener("click", function () {
     // When the player guesses the right number.
     document.querySelector(".body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30 rem";
+
+    document.querySelector(".message").textContent = "Correct Number!";
   }
 });

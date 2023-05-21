5e613fe186ae552da546a96ee53b353867faf044
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-21 13:44:30 -0600

    Displaying the message 'No Number' if the check button is clicked without there being a value in the guess input field.

--- script.js
+++ script.js
@@ -9,4 +9,8 @@ console.log(secretNumber);
 document.querySelector(".check").addEventListener("click", function () {
   // retreiving the number from the guess input field.
   const guess = document.querySelector(".guess").value;
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!";
+  }
 });

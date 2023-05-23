f58d871bdf9467e7c1b527e60da1203d8bc6e70d
Alexander Rivera <alexandercapeta@gmail.com>
2023-05-22 19:23:54 -0600

    Shortening the code for the logic of determining if the guess is too low or too high.

--- script.js
+++ script.js
@@ -25,28 +25,33 @@ document.querySelector(".check").addEventListener("click", function () {
     if (score > highScore) {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
-    } else if (guess > secretNumber) {
-      // When the score is too high
-      if (score > 1) {
-        document.querySelector(".message").textContent = "Guess is Too High!";
-        score--;
-        document.querySelector(".score").textContent = score;
-      } else {
-        document.querySelector(".message").textContent = "You lost the game.";
-        document.querySelector(".score").textContent = 0;
-      }
-    } else if (guess < secretNumber) {
-      // When the guess is too low
-      if (score > 1) {
-        document.querySelector(".message").textContent = "Guess is Too low!";
-        score--;
-        document.querySelector(".score").textContent = score;
-      } else {
-        document.querySelector(".message").textContent = "You lost the game!";
-        document.querySelector(".score").textContent = 0;
-      }
     }
+  } else if (gues !== secretNumber) {
+    displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
+    score--;
+    document.querySelector(".score").textContent = score;
   }
+  // } else if (guess > secretNumber) {
+  //   // When the score is too high
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "Guess is Too High!";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     document.querySelector(".message").textContent = "You lost the game.";
+  //     document.querySelector(".score").textContent = 0;
+  //   }
+  // } else if (guess < secretNumber) {
+  //   // When the guess is too low
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "Guess is Too low!";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     document.querySelector(".message").textContent = "You lost the game!";
+  //     document.querySelector(".score").textContent = 0;
+  //   }
+  // }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
